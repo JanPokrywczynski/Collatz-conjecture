5f2fcb544f0ea4b2b5dc61efcdfc97b898d04ab5
JanPokrywczynski <pokrywczynski.jan@gmail.com>
2022-09-18 20:48:38 +0200

    finalversion

--- CollatzConjecture.py
+++ CollatzConjecture.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
 def collatzConjecture(number):
     result=[]
     number=int(number)
@@ -19,18 +20,17 @@ def showBarPlot(number): #shows how many steps it takes to reach 1 in range 1 to
     plt.ylabel("length")
     plt.xlabel("number")
     plt.autoscale(enable=True)
-    plt.suptitle(f"Collatz conjecture - length from 1 to {number}")
+    plt.suptitle(f"Collatz conjecture\n1 - {number}")
     x0, xmax = plt.xlim()
     y0, ymax = plt.ylim()
     data_width = xmax - x0
     data_height = ymax - y0
-    text = f"max value:\n  {values.index(max(values))+1}"
+    text = f"   longest sequence: {max(values[1::])}\n   for number: {values.index(max(values[1::]))+1}"
     plt.text(x0 + data_width, y0 + data_height, text, fontsize=8)
     plt.show()
 
 def showNormalPlot(number): #shows how many step it takes to reach 1 in range 1 to number in Line chart
     number=int(number)
-    names = [str(i) for i in range(1, number + 1)]
     values = [len(collatzConjecture(i)) for i in range(1, number + 1)]
     values.insert(0,np.nan)
     plt.plot(values,  "b-o", markersize=2, linewidth=0.5)
@@ -47,7 +47,6 @@ def showNormalPlot(number): #shows how many step it takes to reach 1 in range 1 
 
 def showPointPlot(number): #shows how many step it takes to reach 1 in range 1 to number in scatter chart
     number=int(number)
-    names = [str(i) for i in range(1, number + 1)]
     values = [len(collatzConjecture(i)) for i in range(1, number + 1)]
     values.insert(0,np.nan)
     plt.plot(values, "ro", markersize=2)
@@ -64,7 +63,6 @@ def showPointPlot(number): #shows how many step it takes to reach 1 in range 1 t
 
 def showNumberPlot(number): #shows every value number reaches in Collatz conjecture until it reaches 1
     values=collatzConjecture(number)
-    #names=[str(i) for i in range(1, len(values)+1)]
     plt.plot(values, 'r-o')
     plt.suptitle(f"Collatz conjecture\nnumber {number}")
     plt.ylabel("number")
@@ -78,8 +76,8 @@ def showNumberPlot(number): #shows every value number reaches in Collatz conject
     plt.show()
 
 x=input()
-#showBarPlot(x) #slow
-# showNormalPlot(x)
-# showPointPlot(x)
-# showNumberPlot(x)
+showBarPlot(x) #slow
+showNormalPlot(x)
+showPointPlot(x)
+showNumberPlot(x)
 
